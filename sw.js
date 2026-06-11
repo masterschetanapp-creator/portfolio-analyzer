68f783af63b9e99d6172c914d8715ce4e1f60e91
masterschetanapp-creator <masterschetanapp@gmail.com>
2026-06-11 15:11:18 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'masterSchetan-v1';
+const URLS = ['index.html','manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => new Response('Offline',{status:503})))); });
